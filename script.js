3455fa8e94326835669db42b43e5d9f6c8868e5b
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-12-22 21:12:51 +0530

    move boy,box as a global variable

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+var boy = document.createElement("img");
+var box = document.getElementById("box");
+
 function newGame() {
     var btn = document.getElementById("btn-new-game");
     btn.remove();
 
-    var boy = document.createElement("img");
+
     boy.src = "boy/Run (1).png";
     boy.className = "boy";
 
-    var box = document.getElementById("box");
+
     box.appendChild(boy);
 
 }
